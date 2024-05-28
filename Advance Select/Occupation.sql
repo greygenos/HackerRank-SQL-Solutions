e4d6f797194c0c79fc993c20c658dbfec27520cc
Rey Genosa <48041411+greygenos@users.noreply.github.com>
2024-05-28 23:53:57 +0800

    Update Occupation.sql

--- Advance Select/Occupation.sql
+++ Advance Select/Occupation.sql
@@ -1,17 +1,15 @@
-/*
-Mysql solution
-*/
-SELECT 
-MIN(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
-MIN(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
-MIN(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
-MIN(CASE WHEN Occupation = 'Actor' THEN Name ELSE NULL END) AS Actor
-FROM (
-  SELECT a.Occupation,
-         a.Name,
-         (SELECT COUNT(*) 
-            FROM Occupations AS b
-            WHERE a.Occupation = b.Occupation AND a.Name > b.Name) AS rank
-  FROM Occupations AS a
-) AS c
-GROUP BY c.rank;
+SET @d=0, @a=0, @p=0, @s=0;
+SELECT MIN(Doctor),MIN(Professor),MIN(SINGER),MIN(Actor)
+FROM
+(SELECT IF(OCCUPATION='Actor',NAME,NULL) AS Actor,
+        IF(OCCUPATION='Doctor',NAME,NULL) AS Doctor,
+        IF(OCCUPATION='Professor',NAME,NULL) AS Professor,
+        IF(OCCUPATION='Singer',NAME,NULL) AS SINGER,
+ CASE OCCUPATION
+    WHEN 'Actor' THEN @a:=@a+1
+    WHEN 'Doctor' THEN @d:=@d+1
+    WHEN 'Professor' THEN @p:=@p+1
+    WHEN 'Singer' THEN @s:=@s+1
+ END
+AS idn FROM OCCUPATIONS ORDER BY NAME )
+AS temp GROUP BY temp.idn;
